ae39bc1b32a13e6b1dd23f7feda5109411991205
sendo-kakeru <sendokakeru.js@gmail.com>
2024-03-20 22:28:42 +0900

    first commit

---
+++ api/prisma/migrations/20240320132441_v0/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "post" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "post_pkey" PRIMARY KEY ("id")
+);
